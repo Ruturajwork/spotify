15d828f03c6c4b602dd1bacabd078a66476639ae
Ruturaj Salunkhe <103729113+Ruturajwork@users.noreply.github.com>
2022-08-10 02:15:14 +0530

    Add files via upload

---
+++ spotify/src/components/Config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "d984dc138a2d48e8af3d750730c92b48";
+export const redirectUri = "https://spotifytask.vercel.app/redirect";
+export const scopes = [
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
